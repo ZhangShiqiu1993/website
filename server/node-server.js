7ea630f0b6fa156ed74a9b13b596a7998d5d3dd0
zhangshiqiu <zhangshiqiu1993@126.com>
2016-04-15 09:51:06 -0400

    Inserting data into DB

--- server/node-server.js
+++ server/node-server.js
@@ -1,15 +1,37 @@
 var net = require('net');
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://127.0.0.1:27001/test';
+
 
 function listening (socket) {
         socket.on('data',function(data){
-                // socket.write(data.toString()+' GET');
-                fs.appendFile('./geo_data.log',data.toString()+"\n",{flags:"a+",autoClose:true},function (err){
-                        if (err) {
-                                console.log(err);
-                        };
+                var tmp = data.toString().split("\n");
+                var data = tmp[tmp.length-1].split(",");
+                // fs.appendFile('./geo_data.log',
+                //     new Date(Date.now()).toString()+" : "+tmp[tmp.length-1]+"\n--\n",
+                //     {flags:"a+",autoClose:true},
+                //     function (err){
+                //         if (err) {
+                //                 console.log(err);
+                //         };
+                //  });
+
+                MongoClient.connect(url, function (err, db) {
+                    assert.equal(null, err);
+                    var buses = db.collection('buses');
+                    buses.insertOne({
+                        bus_id: data[0],
+                        time: new Date(Date.now()),
+                        next_stop: data[1],
+                        location:{
+                            longitude: data[2],
+                            latitude: data[3]
+                        }
+                    });
                 });
-                socket.write(data.toString()+' GET');
+                socket.write(data.toString());
         });
         socket.on('end', function(){
                 console.log("disconnect");
